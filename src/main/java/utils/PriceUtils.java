39fb7385a3764dc13fa61efbfc4b6dc4513f06f4
Recreation34 <fatihparlayan34@gmail.com>
2018-12-17 17:49:19 +0300

    Get prices from HitBTC Exchange

---
+++ src/main/java/utils/PriceUtils.java
@@ -0,0 +1,35 @@
+package utils;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class PriceUtils {
+
+    public static final String EXCHANGE_URL = "https://api.hitbtc.com/api/2/public/ticker";
+    private static String result;
+    private static int lastUpdate;
+
+    public static String getPricesFromExchange() {
+        int now = (int) (System.currentTimeMillis() / 1000);
+        if (now - lastUpdate > 10) {
+            result = UnirestUtils.getResult(EXCHANGE_URL);
+            lastUpdate = now;
+            System.out.println("prices updated!");
+        }
+        return result;
+    }
+
+    public static double getPrice(String json, String symbol) {
+        symbol = symbol + "USD";
+        JSONArray jsonArray = new JSONArray(json);
+        if (jsonArray.length() > 0) {
+            for (Object object : jsonArray) {
+                JSONObject coinJson = (JSONObject) object;
+                if (symbol.equals(coinJson.getString("symbol"))) {
+                    return coinJson.getDouble("last");
+                }
+            }
+        }
+        return 0.0;
+    }
+}
